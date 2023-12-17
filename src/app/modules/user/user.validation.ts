67ea871dcd34a943f3e47e1968821dad8b404299
Abul Ala Jobayar <jobayar59@gmail.com>
2023-12-17 12:05:55 +0600

    user Validatio Zod create

---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,30 @@
+import { z } from 'zod';
+const fullNameValidation = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+});
+
+const addressValidation = z.object({
+  street: z.string(),
+  city: z.string(),
+  country: z.string(),
+});
+const ordersValidation = z.object({
+  productName: z.string().optional(),
+  price: z.number().optional(),
+  quantity: z.number().optional(),
+});
+
+const userValidation = z.object({
+  userId: z.number(),
+  username: z.string(),
+  password: z.string(),
+  fullName: fullNameValidation,
+  age: z.number(),
+  email: z.string().email('please provide valid email'),
+  isActive: z.boolean().default(true),
+  hobbies: z.array(z.string()),
+  address: addressValidation,
+  orders: ordersValidation.optional(),
+});
+export default userValidation;
